32184495fc2286d121949bbc5c1e656247cadab0
jadychuang <schuang@cca.edu>
2015-11-14 22:23:00 -0800

    website

--- project3/assets/feet/assets/project3.js
+++ project3/assets/feet/assets/project3.js
@@ -24,7 +24,27 @@ $(document).ready(function(){
 
 	$('.letters').html(letterArray);
 
+	// Word shuffle
 
+	$('button').click(function(){
+		$('.letters').shuffle();
+	});
+
+	(function($){
+
+	    $.fn.shuffle = function() {
+	        return this.each(function(){
+	            var items = $(this).children().clone(true);
+	            return (items.length) ? $(this).html($.shuffle(items)) : this;
+	        });
+	    }
+	    
+	    $.shuffle = function(arr) {
+	        for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+	        return arr;
+	    }
+	    
+	})(jQuery);
 
 // This is the closing bracket, parenthesis, and semicolon
 });
